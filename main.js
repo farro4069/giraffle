6bf1757f840ad9b2ebf5de8bcd94542a03d87723
Paul Farrington <farro4069@gmail.com>
2022-02-12 21:19:50 +1000

    Testing numbers

--- main.js
+++ main.js
@@ -240,8 +240,8 @@ function todaysWord() {
 function todaysWordle() {
 	const today = new Date();
 	const wordleStartDate = new Date('2021, 6, 19');
-	const wordleNumber = Math.floor(((today.valueOf() - wordleStartDate.valueOf())/1000/60/60/24));
-	return wordleNumber;
+	const wordleDays = Math.floor(((today.valueOf() - wordleStartDate.valueOf())/1000/60/60/24));
+	return wordleDays;
 }
 
 // ************************************************************
