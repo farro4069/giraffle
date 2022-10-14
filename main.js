5199bdde00c7ece6d411ef0ee42f8a747e2515f9
Paul Farrington <farro4069@gmail.com>
2022-10-14 19:21:04 +0200

    Adjusting NYT19

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /** to find the nytAdjustment >  allWords[ wordleNumber + 18 ] **/ 
 
-const nytAdjustment = 18;
+const nytAdjustment = 19;
 
 const logo = document.querySelector('.logo');
 const instruct = document.querySelector('.btn-info');
